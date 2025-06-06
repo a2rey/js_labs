2e8683b39df1f1251d2afaf825d741193a2035bc
Alvaro <alvreydu@gmail.com>
2025-06-06 19:18:29 -0400

    Experience textarea

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -7,7 +7,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
-    
+    const userExp = document.getElementById("userExperience").value;
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
     document.getElementById('userEmail').innerHTML = email;
@@ -15,6 +15,7 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userEX').innerHTML = userExp;
     document.getElementById('userInfo').style.display = 'block';
 }
 const submitButton = document.getElementById('submitBtn');
